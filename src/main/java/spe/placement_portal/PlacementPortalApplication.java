5cb19a7d91dd5c0745f3f836d0b0a751147d275c
Satyam Khare <39805660+kh-satyam@users.noreply.github.com>
2019-04-07 09:59:22 +0530

    First Commit

---
+++ src/main/java/spe/placement_portal/PlacementPortalApplication.java
@@ -0,0 +1,13 @@
+package spe.placement_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementPortalApplication.class, args);
+	}
+
+}
